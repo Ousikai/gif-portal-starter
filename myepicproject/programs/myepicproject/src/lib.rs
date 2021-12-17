5ea744b10cc3f03875102867b523d518541e79b8
Ousikai <mtfallsVR@protonmail.com>
2021-12-17 16:31:18 -0500

    update to devnet

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("5fc4RHft5e8TBLzZMNSKaMHRZCNS9TDdRcapB9XoqgbY");
 
 #[program]
 pub mod myepicproject {
